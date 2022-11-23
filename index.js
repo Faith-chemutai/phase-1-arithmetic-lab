cc12277edc8a2685e0ccceee00bf3594a79b2775
Faith <faithchemutai2330@gmail.com>
2022-11-23 20:41:28 +0300

    completed arithmentic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 *num2)
+console.log(multiply);
 
-// Write your code here
+ let random = 20;
+ Math.random;
+
+ const num3 = 24;
+ const num4 = 5;
+ let mod = (num3 %num4)
+ console.log(mod);
+
+ let max = 20;
+ Math.max;
+
+
